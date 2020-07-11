37a0b32a27812d7a836269cc1cbb6b9df8e87ec0
Arturo Chen <art.can.fly@gmail.com>
2020-07-11 02:38:24 -0400

    Included 5+ to be colored green

--- CanadaComputers/TamperCC-Arturo.js
+++ CanadaComputers/TamperCC-Arturo.js
@@ -11,46 +11,46 @@
 
 (function () {
 
-	const available_color = "green"
-	const unavailable_color = "#CCCCCC"
+  const available_color = "green"
+  const unavailable_color = "#CCCCCC"
 
-	const stores = [
-		"Greenfield Park",
-		"Laval",
-		"Montréal Downtown",
-		"West Island"
-	]
+  const stores = [
+    "Greenfield Park",
+    "Laval",
+    "Montréal Downtown",
+    "West Island"
+  ]
 
-	const displayStockLevel = () => {
+  const displayStockLevel = () => {
 
-		let product_list_container = document.querySelector("#product-list")
-		let product_list = product_list_container.querySelectorAll(".stocklevel-pop")
-		let stockPopup = product_list_container.querySelectorAll(".stock-popup")
+    let product_list_container = document.querySelector("#product-list")
+    let product_list = product_list_container.querySelectorAll(".stocklevel-pop")
+    let stockPopup = product_list_container.querySelectorAll(".stock-popup")
 
-		for (let index = 0; index < stockPopup.length; index++) {
-			let container = document.createElement('div')
-			for (let storeName of stores) {
+    for (let index = 0; index < stockPopup.length; index++) {
+      let container = document.createElement('div')
+      for (let storeName of stores) {
 
-				let currentStockPopup = stockPopup[index]
-				currentStockPopup.style.display = 'flex'
-				currentStockPopup.style.flexDirection = 'column'
-				currentStockPopup.style.border = '1px solid'
-				currentStockPopup.style.padding = '5px 10px'
+        let currentStockPopup = stockPopup[index]
+        currentStockPopup.style.display = 'flex'
+        currentStockPopup.style.flexDirection = 'column'
+        currentStockPopup.style.border = '1px solid'
+        currentStockPopup.style.padding = '5px 10px'
 
-				let store = document.createElement('span')
-				let productText = product_list[index].innerText
-				let indexStore = productText.indexOf(storeName)
-				let productTextSlice = productText.slice(indexStore)
-				let amountInStock = productTextSlice.match(/(-|\d\D|\d)/i)[0]
+        let store = document.createElement('span')
+        let productText = product_list[index].innerText
+        let indexStore = productText.indexOf(storeName)
+        let productTextSlice = productText.slice(indexStore)
+        let amountInStock = productTextSlice.match(/(-|\d\D|\d)/i)[0]
 
-				store.style.color = amountInStock > 0 ? available_color : unavailable_color
-				store.innerText = `${storeName}: ${amountInStock}`
-				container.appendChild(store)
-				currentStockPopup.innerHTML = container.innerHTML
-			}
-		}
-	}
+        store.style.color = amountInStock !== '-' ? available_color : unavailable_color
+        store.innerText = `${storeName}: ${amountInStock}`
+        container.appendChild(store)
+        currentStockPopup.innerHTML = container.innerHTML
+      }
+    }
+  }
 
-	window.addEventListener("load", displayStockLevel)
-	window.addEventListener("scroll", displayStockLevel)
+  window.addEventListener("load", displayStockLevel)
+  window.addEventListener("scroll", displayStockLevel)
 })()
