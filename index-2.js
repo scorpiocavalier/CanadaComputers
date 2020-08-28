26b0bb909d6f5447549b5adad4976a7655f32d5a
Arturo Chen <art.can.fly@gmail.com>
2020-08-28 02:30:44 -0400

    * fixed items with stock level 5+ & * styling

--- index-2.js
+++ index-2.js
@@ -18,6 +18,7 @@
   ]
 
   const getProducts = () => document.querySelector('#product-list')
+  const getProductsArray = () => document.querySelectorAll('.productTemplate')
   const getSource = products => products.querySelectorAll('.stocklevel-pop')
   const getDestination = products => products.querySelectorAll('.stock-popup')
 
@@ -27,8 +28,11 @@
   const styleDestination = dest => {
     dest.style.display = 'flex'
     dest.style.flexDirection = 'column'
+    dest.style.justifyContent = 'center'
+    dest.style.alignItems = 'center'
     dest.style.border = '2px solid #00498e'
     dest.style.padding = '5px 10px'
+    dest.style.height = '108px'
   }
 
   const getStockLevel = (store, source, index) => {
@@ -52,6 +56,8 @@
 
     // list of all products displayed
     const products = getProducts()
+    // array of products
+    const productsArr = getProductsArray(products)
     // get inventory info source
     const source = getSource(products)
     // get container to put info in
@@ -70,16 +76,31 @@
       stores.forEach(store => {
         const stockLevel = getStockLevel(store, source, index)
 
-        if (stockLevel > 0) {
+        if (stockLevel == '5+' | stockLevel > 0) {
           const storeSpan = createSpan()
           store = getStoreNickname(store)
           storeSpan.innerText = `${ store }: ${ stockLevel }`
           container.appendChild(storeSpan)
           dest.innerHTML = container.innerHTML
+
+          // [Optional] Border on in stock items
+          productsArr[ index ].style.border = '2px solid #00498e'
+          productsArr[ index ].style.borderRadius = '15px'
         }
       })
+
+      if (container.childElementCount == 0) {
+        // set product visibility: [0 = not visible ~ 1 = visible]
+        productsArr[ index ].style.opacity = '0.2'
+
+        // empty description of stock level
+        dest.innerHTML = ''
+      }
     })
   }
+
+  // [Optional] Display items as list by default
+  // window.addEventListener("load", toggleToList())
 
   window.addEventListener("load", displayStockLevel)
   window.addEventListener("scroll", displayStockLevel)
